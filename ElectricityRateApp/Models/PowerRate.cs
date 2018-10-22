cb458dc9a6f4b4abea4a91f030a6e8e7156d550a
Ryan Sallee <rcsall11@g.holycross.edu>
2018-10-21 21:32:35 -0400

    Added model for PowerRate.

---
+++ ElectricityRateApp/Models/PowerRate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricityRateApp.Models
+{
+    public class PowerRate
+    {
+        public PowerRate(string zipCode, string utilityName, double commericalRate, double residentialRate)
+        {
+            ZipCode = zipCode;
+            UtilityName = utilityName;
+            CommercialRate = commericalRate;
+            ResidentialRate = residentialRate;
+        }
+        public int Id { get; set; }
+        public string ZipCode { get; private set; }
+        public string UtilityName { get; private set; }
+        public double CommercialRate { get; private set; }
+        public double ResidentialRate { get; private set; }
+    }
+}
