33263ae1209ed340500b3eddc46158a177615118
Ryan Sallee <rcsall11@g.holycross.edu>
2018-11-05 19:22:17 -0500

    Added ConsoleTable package to simply displaying results in a readable table. Updated all methods to use the ConsoleTable package in SearchResults class.

--- ElectricityRateApp/GetAndCalculate/SearchResults.cs
+++ ElectricityRateApp/GetAndCalculate/SearchResults.cs
@@ -3,6 +3,7 @@ using ElectricityRateApp.Data;
 using ElectricityRateApp.Models;
 using System.Linq;
 using System.Collections.Generic;
+using ConsoleTables;
 
 namespace ElectricityRateApp.GetAndCalculate
 {
@@ -18,27 +19,19 @@ namespace ElectricityRateApp.GetAndCalculate
                 return;
             }
             Console.WriteLine(string.Format("Here are the last {0} results", numberOfResults));
-
+            var table = new ConsoleTable("Time", "City", "State", "Provider");
             using (var context = new ElectricityRatesContext())
             {
                 var results = context.ProviderSearchResults.OrderByDescending(r => r.Id)
-                    .Take(numberOfResults);
-
-                Console.WriteLine("Time \t \t City \t \t State \t Provider");
+                    .Take(numberOfResults);                             
+                
                 foreach (var result in results)
-                {
-                    if (result.City.Length <= 5)
-                    {
-                        Console.WriteLine(string.Format("{0} \t {1} \t\t {2} \t {3}", result.Time,
-                        result.City, result.StateAbbreviation, result.ProviderName));
-                    }
-                    else
-                    {
-                        Console.WriteLine(string.Format("{0} \t {1} \t {2} \t {3}", result.Time,
-                            result.City, result.StateAbbreviation, result.ProviderName));
-                    }
-                }
+                {      
+                    table.AddRow(result.Time, result.City, result.StateAbbreviation, result.ProviderName);
+                }                
             }
+            table.Write();
+            Console.WriteLine();
         }
 
         public static void GetChargeCalcuationHistory()
@@ -52,25 +45,20 @@ namespace ElectricityRateApp.GetAndCalculate
             }
 
             Console.WriteLine(string.Format("Here are the last {0} results:", numberOfResults));
+            var table = new ConsoleTable("Time", "City", "State", "Rate", "Charge", "Usage(kWh)");
             using (var context = new ElectricityRatesContext())
             {
                 var results = context.ResidentialChargeResults.OrderByDescending(r => r.Id)
                     .Take(numberOfResults);
-                Console.WriteLine("Time \t \t City \t \t State \t Rate \t Charge\t Usage(kwh)");
+                                
                 foreach (var result in results)
                 {
-                    if (result.City.Length <= 5)
-                    {
-                        Console.WriteLine(string.Format("{0} \t {1} \t\t {2} \t {3:C} \t{4:C} \t {5}",
-                        result.Time, result.City, result.StateAbbreviation, result.Rate, result.Charge, result.Usage));
-                    }
-                    else
-                    {
-                        Console.WriteLine(string.Format("{0} \t {1} \t {2} \t {3:C} \t{4:C} \t {5}",
-                            result.Time, result.City, result.StateAbbreviation, result.Rate, result.Charge, result.Usage));
-                    }
-                }
+                    table.AddRow(result.Time, result.City, result.StateAbbreviation, 
+                        string.Format("{0:C}",result.Rate), string.Format("{0:C}", result.Charge), result.Usage);
+                }                
             }
+            table.Write();
+            Console.WriteLine();
         }
 
         public static void GetRateComparisonHistory()
@@ -84,19 +72,21 @@ namespace ElectricityRateApp.GetAndCalculate
             }
 
             Console.WriteLine(string.Format("Here are the last {0} results:", numberOfResults));
+            Console.WriteLine("A negative Percetage in the Difference Column means the first city's rate is less");
+            var table = new ConsoleTable("Time", "City 1", "State 1", "Rate 1", "City 2", "State 2", "Rate 2", "Difference");
             using (var context = new ElectricityRatesContext())
             {
                 var results = context.RateComparisonResults.OrderByDescending(r => r.Id)
                     .Take(numberOfResults);
 
-                Console.WriteLine();
                 foreach (var result in results)
                 {
-                    Console.WriteLine(string.Format("{0} \t {1} \t\t {2} \t {3:C} \t{4} \t {5} \t{6:C} \t {7:P2}",
-                        result.Time, result.City1, result.StateAbbreviation1, result.Rate1,
-                        result.City2, result.StateAbbreviation2, result.Rate2, result.Difference));
+                    table.AddRow(result.Time, result.City1, result.StateAbbreviation1, string.Format("{0:C}", result.Rate1),
+                        result.City2, result.StateAbbreviation2, string.Format("{0:C}", result.Rate2), string.Format("{0:P2}",result.Difference));
                 }
             }
+            table.Write();
+            Console.WriteLine();
         }
     }
 }
